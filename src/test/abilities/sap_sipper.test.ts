a3d04dcedc644ead2f2c9071e1b558908eadf067
Matthew Olker <matthew.olker@gmail.com>
2024-06-12 22:06:50 -0400

    fix sap sipper test import

--- src/test/abilities/sap_sipper.test.ts
+++ src/test/abilities/sap_sipper.test.ts
@@ -2,16 +2,14 @@ import {afterEach, beforeAll, beforeEach, describe, expect, it, vi} from "vitest
 import Phaser from "phaser";
 import GameManager from "#app/test/utils/gameManager";
 import * as overrides from "#app/overrides";
-import {Species} from "#app/data/enums/species";
 import {
   CommandPhase,
   EnemyCommandPhase, TurnEndPhase,
 } from "#app/phases";
 import {Mode} from "#app/ui/ui";
-import {Moves} from "#app/data/enums/moves";
 import {getMovePosition} from "#app/test/utils/gameManagerUtils";
 import {Command} from "#app/ui/command-ui-handler";
-import { Abilities } from "#app/data/enums/abilities.js";
+import { Abilities, Moves, Species } from "#enums";
 import { BattleStat } from "#app/data/battle-stat.js";
 import { TerrainType } from "#app/data/terrain.js";
 
